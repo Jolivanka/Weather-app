3eb1bdd73bd4d2d4dd9a4b1f951fbec76a1fb2b6
Jolivanka <123731252+Jolivanka@users.noreply.github.com>
2023-11-22 14:59:06 +0100

    Update apis.js

--- src/components/apis.js
+++ src/components/apis.js
@@ -1,4 +1,4 @@
-export const APIkey = "9df45b05d5b620870b333c1e23840265";
+export const APIkey = "Your API code here";
 export const APIurl = "https://api.openweathermap.org/data/2.5/";
 
 export const userLocationApi = `http://api.openweathermap.org/geo/1.0/`;
